5dbea157ae4490bcb5855a6511dacd0915e0ec71
Kaan Şarkaya <kaansarkaya@gmail.com>
2023-04-18 08:00:06 +0300

    change config for build error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        loader: "akamai",
-        path: "",
-    },
     assetPrefix: "./",
 
     experimental: {
         appDir: true,
     },
+    output: "export",
 };
 
 module.exports = nextConfig;
